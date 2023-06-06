b6add8d7929c94283474de2a3e18e6d52c2a27bb
Ritesh Patel <rp214322@gmail.com>
2023-06-06 21:17:04 +0530

    sidebar complete

--- client/src/components/RightSidebar/RightSidebar.jsx
+++ client/src/components/RightSidebar/RightSidebar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "./RightSidebar.css";
+import Widget from "./Widget";
+import WidgetTags from "./WidgetTags";
 
 const RightSidebar = () => {
     return (
-        <div>
-
-        </div>
+        <aside className="right-sidebar">
+            <Widget />
+            <WidgetTags />
+        </aside>
     )
 }
 
